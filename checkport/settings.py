c65478c044d39b66d6c122665e3c55fabcecedcc
pengng <pengng@fenbi.com>
2018-09-25 17:42:04 +0800

    statics->static

--- checkport/settings.py
+++ checkport/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
                     )
-#STATIC_ROOT = 'statics'
+#STATIC_ROOT = 'static'
 
 
 BROKER_URL = 'redis://127.0.0.1:6379/9'
